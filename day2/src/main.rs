550732b4307a93e911dd11c3ebfb164c7fda42fd
The-Minecraft-Scientist <charlesliske@gmail.com>
2022-12-01 21:35:55 -0800

    day2!

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,69 @@
 #[macro_use]
 extern crate load_file;
 
-fn main() {
-    let file_in = load_str!("foo.txt");
-    println!("{}", file_in);
+use std::collections::HashMap;
+use crate::Weapon::{Paper, Rock, Scissors};
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+pub enum Weapon {
+    Rock(),
+    Paper(),
+    Scissors(),
 }
+
+impl Weapon {
+    fn get_score(&self) -> u32 {
+        match self {
+            Weapon::Rock() => {1}
+            Weapon::Paper() => {2}
+            Weapon::Scissors() => {3}
+        }
+    }
+    fn get_weakness(&self) -> Weapon {
+        match self {
+            Rock() => {Paper()}
+            Paper() => {Scissors()}
+            Scissors() => {Rock()}
+        }
+    }
+    fn get_win(&self) -> Weapon {
+        match self {
+            Paper() => {Rock()}
+            Scissors() => {Paper()}
+            Rock() => {Scissors()}
+        }
+    }
+
+}
+fn main() {
+    let mut map: HashMap<char, Weapon> = HashMap::default();
+    map.insert('A', Rock());
+    map.insert('B', Paper());
+    map.insert('C', Scissors());
+    let file_in = load_str!("foo2.txt");
+    let mut score = 0u32;
+    for item in file_in.split("\n") {
+        let they_played = map.get(&item.chars().nth(0).unwrap()).unwrap();
+        let needed_ending = &item.chars().nth(2).unwrap();
+        let you_played;
+        match needed_ending {
+            'X' => {you_played = they_played.get_win()},
+            'Y' => {you_played = they_played.clone()},
+            'Z' => {you_played = they_played.get_weakness()},
+            _ => {panic!("this shouldnt happen")}
+        }
+        dbg!((you_played,they_played));
+        score += you_played.get_score();
+        if you_played == they_played.clone() {
+            score += 3;
+            println!("tie")
+        } else if they_played.get_weakness() == you_played.clone() {
+            score += 6;
+            println!("you won")
+        } else {
+            println!("you lost")
+        }
+    }
+    println!("{:?}", score);
+}
+
+
