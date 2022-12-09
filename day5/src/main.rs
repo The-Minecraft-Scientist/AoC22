aaf656f6d3fe5d5751b95d0453466fecefbb15ff
The-Minecraft-Scientist <charlesliske@gmail.com>
2022-12-08 16:37:22 -0800

    days 5 and 6 (certified grindset)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,7 +1,43 @@
 #[macro_use]
 extern crate load_file;
 
+use std::str::FromStr;
+
 fn main() {
+    let mut stacks = vec![];
+    stacks.push(vec!["R", "N", "P", "G"]);
+    stacks.push(vec!["T", "J", "B", "L", "C", "S", "V", "H"]);
+    stacks.push(vec!["T", "D", "B", "M", "N", "L"]);
+    stacks.push(vec!["R", "V", "P", "S", "B"]);
+    stacks.push(vec!["G", "C", "Q", "S", "W", "M", "V", "H"]);
+    stacks.push(vec!["W", "Q", "S", "C", "D", "B", "J"]);
+    stacks.push(vec!["F", "Q", "L"]);
+    stacks.push(vec!["W", "M", "H", "T", "D", "L", "F", "V"]);
+    stacks.push(vec!["L", "P", "B", "V", "M", "J", "F"]);
+   /* stacks.push(vec!["Z", "N"]);
+    stacks.push(vec!["M", "C", "D"]);
+    stacks.push(vec!["P"]);*/
     let file_in = load_str!("foo.txt");
-    println!("{}", file_in);
+    let split: Vec<Vec<&str>> = file_in.split("\n").map(|x| -> Vec<&str> {
+        x.split(" ").collect::<Vec<&str>>()
+    }).collect();
+    for line in split {
+        println!("{:?}", &line);
+        dbg!(&stacks);
+        let from = u32::from_str(line[3]).unwrap() as usize -1;
+        let final_index = (&stacks[from].len() - 1) - (u32::from_str(line[1]).unwrap() as usize - 1);
+        let to = u32::from_str(line[5]).unwrap() as usize -1;
+        dbg!(&from);
+        dbg!(&final_index);
+        dbg!(&to);
+
+        let mut split_off = stacks[from].split_off(final_index);
+        //part one
+        // split_off.reverse();
+        stacks[to].append(&mut split_off);
+    }
+    for mut stack in stacks {
+        print!("{}", stack.pop().unwrap())
+    }
 }
+
--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,6 +2,24 @@
 extern crate load_file;
 
 fn main() {
-    let file_in = load_str!("foo.txt");
-    println!("{}", file_in);
+    let s = load_str!("foo.txt").chars().collect::<Vec<char>>();
+    let mut i = 0usize;
+    loop {
+        if i + 14 >= (s.len() - 1) {break}
+        let c = &s[i..i+14];
+        if all_unique(c.to_vec()) {
+            break;
+        }
+        i += 1;
+    };
+    println!("{}", i + 14);
+}
+fn all_unique(rhs: Vec<char>) -> bool {
+    let mut out = true;
+    for item in rhs.iter() {
+        if rhs.iter().filter(|&x| x == item).count() != 1 {
+            out = false;
+        }
+    }
+    out
 }
